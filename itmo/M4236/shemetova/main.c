e5915b3e11eae3dcf099b7f4e57e8b1b08401e83
katyacyfra <katyacyfra@gmail.com>
2019-01-10 19:05:56 +0300

    opencl lab-2 ready

--- itmo/M4236/shemetova/main.c
+++ itmo/M4236/shemetova/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
 #include <math.h>
 
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
@@ -9,66 +11,27 @@
 #include <CL/cl.h>
 #endif
 
-#define MAX_SOURCE_SIZE (0x100000)
 
-int closest_size(int size, int block) {
-    return ceil(size/(float) block)*block;
+#define MAX_SOURCE_SIZE (0x100000)
+#define BLOCK (256)
+
+int closest_size(int size) {
+    return ceil(size/(float) BLOCK)*BLOCK;
 }
 
-int main(int argc, char* argv[]) {
-	
-	if (argc < 2) {
-		printf("Error: Please, specify input file!");
-        exit(1);
-	}
-	
-	int const MAX_N = 1024;
-	int const MAX_M = 9;
-	size_t const MAX_BLOCK = 16;
-    // Initialize the two input matrices
-    FILE* file = fopen(argv[1], "r"); 
-
-    if (file == NULL){
-        printf("Error Reading File\n");
-        exit (0);
-    }
-	
-	int N, M;
-	int sc;
-	int scanf_res;
-	
-	scanf_res = fscanf(file, "%d,", &N);
-	scanf_res = fscanf(file, "%d,", &M);
-	
-	
-	float* A = (float *)malloc(N * N * sizeof(float));
-	float* B = (float *)malloc(M * M * sizeof(float));
-	float* C = (float *)malloc(N * N * sizeof(float));
-	
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < N; ++j) {
-            sc = fscanf(file, "%f", &A[i*N + j]);
-       }
-	}
-	
-	for (int i = 0; i < M; ++i) {
-		for (int j = 0; j < M; ++j) {
-            sc = fscanf(file, "%f", &B[i*M + j]);
-       }
-	}
+void block_sum(float* in, float* B, const int N) {
+	// Load the kernel source code into the array source_str
 
 	
-    fclose(file);
+	//binding to BLOCK size
+	float* A;
+	int global = closest_size(N);
 	
-	int HM = (M - 1)/2;
-
-
-    // Load the kernel source code into the array source_str
     FILE *fp;
     char *source_str;
     size_t source_size;
 
-    fp = fopen("matrix_conv.cl", "r");
+    fp = fopen("prefix_summ.cl", "r");
     if (!fp) {
         fprintf(stderr, "Failed to load kernel.\n");
         exit(1);
@@ -99,17 +62,13 @@ int main(int argc, char* argv[]) {
 
     // Create memory buffers on the device for each vector 
     cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, 
-            MAX_N * MAX_N * sizeof(float), NULL, &ret);
-    cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
-            MAX_M * MAX_M * sizeof(float), NULL, &ret);
-    cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, 
-            MAX_N * MAX_N * sizeof(float), NULL, &ret);
+            N * sizeof(float), NULL, &ret);
+    cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, 
+            N * sizeof(float), NULL, &ret);
 
-    // Copy the lists A and B to their respective memory buffers
+    // Copy the list in to respective memory buffers
     ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0,
-            N * N * sizeof(float), A, 0, NULL, NULL);
-    ret = clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0, 
-            M * M * sizeof(float), B, 0, NULL, NULL);
+            N *  sizeof(float), in, 0, NULL, NULL);
     if (ret != CL_SUCCESS)
     {
         printf("Error: Failed to copy buffers! %d\n", ret);
@@ -128,69 +87,194 @@ int main(int argc, char* argv[]) {
         printf("Error: Failed to build program! %d\n", ret);
         exit(1);
     }
-
-    // Create the OpenCL kernel
-    cl_kernel kernel = clCreateKernel(program, "matrix_conv", &ret);
-
-
-    // Set the arguments of the kernel
-    ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&a_mem_obj);
-    ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&b_mem_obj);
-    ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&c_mem_obj);
-    ret = clSetKernelArg(kernel, 3, sizeof(int), &N);
-	if (ret != CL_SUCCESS)
-    {
+	
+	 // load named kernel from opencl source
+	 cl_event event = NULL;
+	
+     cl_kernel kernel_hillis = clCreateKernel(program, "scan_hillis_steele", &ret);
+	//https://stackoverflow.com/questions/43542618/how-to-pass-a-array-in-local-address-space-in-opencl-kernel/43581209
+	 ret = clSetKernelArg(kernel_hillis, 0, sizeof(cl_mem), (void *)&a_mem_obj);
+     ret = clSetKernelArg(kernel_hillis, 1, sizeof(cl_mem), (void *)&b_mem_obj);
+	 ret = clSetKernelArg(kernel_hillis, 2, sizeof(float) * BLOCK, NULL),
+     ret = clSetKernelArg(kernel_hillis, 3, sizeof(float) * BLOCK, NULL);
+	 if (ret != CL_SUCCESS)
+     {
         printf("Error: Failed to set args! %d\n", ret);
         exit(1);
-    }
-    ret = clSetKernelArg(kernel, 4, sizeof(int), &M);
-	ret = clSetKernelArg(kernel, 5, sizeof(int), &HM);
-	if (ret != CL_SUCCESS)
-    {
-        printf("Error: Failed to set args! %d\n", ret);
-        exit(1);
-    }
-    
-    // Execute the OpenCL kernel on the list
-	int grid = closest_size(N, MAX_BLOCK);
-	const size_t local[2] = { MAX_BLOCK, MAX_BLOCK };
-    const size_t global[2] = { grid, grid };
-    ret = clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL, 
-            global, local, 0, NULL, NULL);
+     }
+
+    const size_t local = BLOCK;
+    const size_t global_ = global;
+    ret = clEnqueueNDRangeKernel(command_queue, kernel_hillis, 1, NULL, 
+            &global_, &local, 0, NULL, &event);
     if (ret != CL_SUCCESS)
     {
         printf("Error: Failed to retrieve kernel work group info! %d\n", ret);
         exit(1);
     }
 
-
-    // Read the memory buffer C on the device to the local variable C
-    //int *C = (int*)malloc(sizeof(int)*LIST_SIZE);
-    ret = clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0, 
-            N * N * sizeof(float), C, 0, NULL, NULL);
-
-    // Display the result on the screen
-    for(int i = 0; i < N; ++i) {
-		printf("\n");
-       for(int j = 0; j < N; ++j) {
-        printf("%.3f ", C[i*N + j]);
-	   }
-	}
-	printf("\n");
-
-    // Clean up
+    clWaitForEvents(1, &event);
+	
+	ret = clEnqueueReadBuffer(command_queue, b_mem_obj, CL_TRUE, 0, N * sizeof(float), B, 0, NULL, NULL);
+	if (ret != CL_SUCCESS)
+    {
+        printf("Error: Failed to retrieve kernel work group info! %d\n", ret);
+        exit(1);
+    }
+	
+	
     ret = clFlush(command_queue);
     ret = clFinish(command_queue);
-    ret = clReleaseKernel(kernel);
-    ret = clReleaseProgram(program);
+    ret = clReleaseKernel(kernel_hillis);
     ret = clReleaseMemObject(a_mem_obj);
     ret = clReleaseMemObject(b_mem_obj);
-    ret = clReleaseMemObject(c_mem_obj);
     ret = clReleaseCommandQueue(command_queue);
     ret = clReleaseContext(context);
+	
+	
+	if (N <= BLOCK) {
+	   return;
+	
+    }
+    
+	int group_count = 1 + ((N - 1) / BLOCK);
+	
+	
+	float* g_sums = (float *)malloc(group_count * sizeof(float));
+    for (int i = 0; i < group_count; ++i) {
+		if ((i + 1) * BLOCK - 1 >= N) {
+			g_sums[i] = B[N-1];
+		}
+		else {
+            g_sums[i] = B[(i + 1) * BLOCK - 1];
+		}
+    }
+		
+	float* g_sums_result = (float *)malloc(group_count * sizeof(float));
+	block_sum(g_sums,  g_sums_result, group_count);
+
+		
+	//////////////// AGGREGATION ////////////////////////////////	
+	// Create an OpenCL context
+	 ret = clGetDeviceIDs( platform_id, CL_DEVICE_TYPE_ALL, 1, 
+            &device_id, &ret_num_devices);
+	if (ret != CL_SUCCESS)
+    {
+        printf("Error: Failed to build program! %d\n", ret);
+        exit(1);
+    }
+    cl_context context_agg = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
+
+    // Create a command queue
+    cl_command_queue command_queue_agg = clCreateCommandQueue(context_agg, device_id, 0, &ret);
+	
+    // Create memory buffers on the device for each vector 
+    cl_mem in_mem_obj = clCreateBuffer(context_agg, CL_MEM_READ_ONLY, 
+            N * sizeof(float), NULL, &ret);
+	cl_mem gr_mem_obj = clCreateBuffer(context_agg, CL_MEM_READ_ONLY, 
+            group_count * sizeof(float), NULL, &ret);
+    cl_mem out_mem_obj = clCreateBuffer(context_agg, CL_MEM_WRITE_ONLY, 
+            N * sizeof(float), NULL, &ret);
+	
+    // Copy the list B and gr_sums to respective memory buffers
+    ret = clEnqueueWriteBuffer(command_queue_agg, in_mem_obj, CL_TRUE, 0,
+            N *  sizeof(float), B, 0, NULL, NULL);
+	ret = clEnqueueWriteBuffer(command_queue_agg, gr_mem_obj, CL_TRUE, 0,
+            group_count *  sizeof(float), g_sums_result, 0, NULL, NULL);
+	ret = clEnqueueWriteBuffer(command_queue_agg, out_mem_obj, CL_TRUE, 0,
+            group_count *  sizeof(float), B, 0, NULL, NULL);
+    if (ret != CL_SUCCESS)
+    {
+        printf("Error: Failed to copy buffers! %d\n", ret);
+        exit(1);
+    }
+	
+	 // load named kernel from opencl source
+	 event = NULL;
+	
+     cl_kernel kernel_agg = clCreateKernel(program, "aggregate_sums", &ret);
+
+	 ret = clSetKernelArg(kernel_agg, 0, sizeof(cl_mem), (void *)&in_mem_obj);
+	 ret = clSetKernelArg(kernel_agg, 1, sizeof(cl_mem), (void *)&gr_mem_obj);
+     ret = clSetKernelArg(kernel_agg, 2, sizeof(cl_mem), (void *)&out_mem_obj);
+	 if (ret != CL_SUCCESS)
+     {
+        printf("Error: Failed to set args! %d\n", ret);
+        exit(1);
+     }
+
+    ret = clEnqueueNDRangeKernel(command_queue, kernel_agg, 1, NULL, 
+            &global_, &local, 0, NULL, &event);
+    if (ret != CL_SUCCESS)
+    {
+        printf("Error: Failed to run! %d\n", ret);
+        exit(1);
+    }
+
+    clWaitForEvents(1, &event);
+	
+	ret = clEnqueueReadBuffer(command_queue_agg, out_mem_obj, CL_TRUE, 0, N * sizeof(float), B, 0, NULL, NULL);
+	
+
+	if (ret != CL_SUCCESS)
+    {
+        printf("Error: Failed to retrieve result! %d\n", ret);
+        exit(1);
+    }
+		
+	ret = clFlush(command_queue_agg);
+    ret = clFinish(command_queue_agg);
+    ret = clReleaseKernel(kernel_agg);
+	ret = clReleaseMemObject(in_mem_obj);
+    ret = clReleaseMemObject(out_mem_obj);
+	ret = clReleaseMemObject(gr_mem_obj);
+    ret = clReleaseProgram(program);
+
+    ret = clReleaseCommandQueue(command_queue_agg);
+    ret = clReleaseContext(context_agg);
+	free(A);
+}
+
+int main(int argc, char* argv[]) {
+	
+	if (argc < 2) {
+		printf("Error: Please, specify input file!");
+        exit(1);
+	}
+
+    // Initialize the input array
+    FILE* file = fopen(argv[1], "r"); 
+
+    if (file == NULL){
+        printf("Error Reading File\n");
+        exit (0);
+    }
+	
+	int N;
+	int sc;
+	
+	sc = fscanf(file, "%d,", &N);
+	
+	
+	float* A = (float *)malloc(N * sizeof(float));
+	float* B = (float *)malloc(N * sizeof(float));
+	
+	for (int i = 0; i < N; ++i) {
+            sc = fscanf(file, "%f", &A[i]);
+       }
+
+	
+    fclose(file);
+	
+	//recursive count of sums
+	block_sum(A, B, N);
+	
+    for(int j = 0; j < N; ++j) {
+       printf("%.3f ", B[j]);
+	}
+	printf("\n");
     free(A);
     free(B);
-    free(C);
     return 0;
 }
 
